e16fa4e1ba0dd58a209f6604ad40ea03a9b3b514
MZykin <zykininho@gmail.com>
2022-06-12 13:54:55 +0300

    Fix after code-review

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 
 public class DepositCalculator {
 
-    double calculateComplexPercent(double sumOfAmount, double yearRate, int depositPeriod) {
-        // переменную sumOfAmount можно назвать просто amount - здесь нет суммы
-        double pay = sumOfAmount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
-
-        return roundNumber(pay, 2); // перед return вроде не надо ставить пустую строку
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        // переменную sumOfAmount можно назвать просто amount - здесь нет суммы - исправил
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return roundNumber(pay, 2); // перед return вроде не надо ставить пустую строку - да, ставил разрыв,
+                                          // как перед блоком переменных и основного тела метода, но тут точно не нужно
     }
 
     double calculateSimplePercent(double sumOfAmount, double yearRate, int depositPeriod) {
         double pay = sumOfAmount + sumOfAmount * yearRate * depositPeriod;
-
         return roundNumber(pay, 2);
     }
 
     double roundNumber(double value, int places) {
         double scale = Math.pow(10, places);
-
         return Math.round(value * scale) / scale;
     }
 
-    void getFinalDepositAmount() { // мне кажется, что название get.. не подходит, потому что метод ничего не возвращает
+    void calculateDeposit() { // мне кажется, что название get.. не подходит, потому что метод ничего не возвращает
         int period;
         int action;
         double finalSumDeposit = 0;
@@ -42,7 +40,7 @@ public class DepositCalculator {
     }
 
     public static void main(String[] args) {
-        new DepositCalculator().getFinalDepositAmount();
+        new DepositCalculator().calculateDeposit();
     }
 
 }
